de89dc6f4cba730878d571418c02472dc289b868
unknown <jcambrogi62@gmail.com>
2022-09-06 21:11:22 -0300

    MCGA-02: Se creo la base de datos y se conecto en el servidor clouster

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/login', (req, res) => {
+  res.send('login')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.post("./data/", (req, res) => {
+    console.log(req.body)
 })
